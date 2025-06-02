085429522d3210ad83555cc5929e9e53e1318e36
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2025-06-02 15:33:16 +0330

    added product services and controller

---
+++ src/entities/products/product.controller.ts
@@ -0,0 +1,51 @@
+import { Body, Controller, Get, NotFoundException, Param, Post, Req, Res } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Request, Response } from 'express';
+import { CreateReviewDTO } from './dto/createReview.dto';
+
+@Controller('products')
+export class ProductController {
+  constructor(
+    // ...
+    private ProductService: ProductService,
+  ) {}
+
+  @Get('/')
+  async GetProductsWithPagination(@Req() req: Request, @Res() res: Response): Promise<Response | void> {
+    const { results, total, page, pageTotal } = await this.ProductService.GetProductsWithPagination({
+      search: req.query.search ? req.query.search.toString() : '',
+      page: req.query.page ? parseInt(req.query.page.toString()) : 1,
+      pp: req.query.pp ? parseInt(req.query.pp.toString()) : 25, // per page items
+      sortType: req.query.sort_type === 'asc' ? 'asc' : 'desc',
+      sortBy: req.query.sort?.toString() || '',
+    });
+
+    return res.json({ records: results[0], page, total, pageTotal });
+  }
+
+  @Get('/:id')
+  async GetProductById(@Param('id') id: string, @Req() req: Request, @Res() res: Response): Promise<Response | void> {
+    const product = await this.ProductService.GetProductById(id);
+    if (!product) throw new NotFoundException();
+    return res.json(product);
+  }
+
+  @Get('/:id/reviews')
+  async GetProductReviews(@Param('id') id: string, @Req() req: Request, @Res() res: Response): Promise<Response | void> {
+    // ...
+  }
+
+  @Post('/:id/reviews')
+  async CreateReviewForProduct(@Param('id') id: string, @Body() body: CreateReviewDTO, @Req() req: Request, @Res() res: Response): Promise<Response | void> {
+    const product = await this.ProductService.CreateReviewForProduct(id, body.text, body.rating, body.usersName);
+  }
+
+  @Get('/:id/summarize')
+  async GetSummarizeInfo(@Param('id') id: string, @Req() req: Request, @Res() res: Response): Promise<Response | void> {
+    const type: 'reviews' | 'description' = req.query.type?.toString() == 'reviews' ? 'reviews' : 'description';
+    const product = await this.ProductService.GetProductById(id);
+    if (!product) throw new NotFoundException();
+
+    // ...
+  }
+}
---
+++ src/entities/products/product.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { WithPaginationServiceParams } from 'src/interfaces/query.interfaces';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    // ...
+    public PrismaService: PrismaService,
+  ) {}
+
+  async GetAllProducts() {
+    return await this.PrismaService.onModuleInit();
+  }
+
+  async GetProductsWithPagination(params: WithPaginationServiceParams) {
+    const results = [];
+    const total = 0;
+    const pageTotal = 0;
+
+    return { results, total, page: params.page, pageTotal };
+  }
+
+  async GetProductById(id: string) {
+    return await this.PrismaService.products.findFirst({
+      where: { id },
+    });
+  }
+
+  async GetProductReviews(id: string) {
+    return await this.PrismaService.productReviews.findMany({
+      where: { productId: id },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  async CreateReviewForProduct(id: string, text: string, rating: number, usersName: string) {
+    return await this.PrismaService.productReviews.create({
+      data: {
+        reviewText: text,
+        rating: rating,
+        product: { connect: { id } },
+      },
+    });
+  }
+}
