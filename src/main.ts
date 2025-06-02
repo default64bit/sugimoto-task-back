8a3db00d4a3c07ecd426ea0de6a775b2bca22604
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2025-06-02 13:54:59 +0330

    added custonm settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // adds validation pipe customizations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        return new UnprocessableEntityException(
+          errors.map((item) => {
+            return {
+              property: item.property,
+              errors: item.constraints && Object.values(item.constraints),
+            };
+          }),
+        );
+      },
+    }),
+  );
+
+  // yum, yum cookie parser
+  app.use(cookieParser());
+
+  // make them CORS happen
+  app.enableCors({ origin: process.env.FRONT_URL });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
